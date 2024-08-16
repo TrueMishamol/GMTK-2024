98b231f57a61eaf3d2aecff04388588d2ad1e4f5
WatermelonFroggy <82559263+WatermelonFroggy@users.noreply.github.com>
2024-08-16 15:42:50 -0400

    Most base stuff finished, more to come soon

---
+++ nodes/resources/towerFunctionControllers/tower_function_base.gd
@@ -0,0 +1,31 @@
+extends Resource
+class_name towerFunction
+
+
+@export_group("Part Stats")
+# Name of the part that will appear in UI
+@export var partName:String
+# Current level of the part
+@export var partLevel:int = 0
+# Maximum level the part can reach
+@export var maxPartLevel:int = 3
+
+@export_group("Upkeep Stats")
+# Price to upkeep the part per in-game day
+@export var upkeepPrice:int = 0
+
+@export_group("Defense Stats")
+# Current defense level of the part
+@export var defenseLevel:int = 0
+# Maximum defense level the part can reach
+@export var maxDefenselevel:int = 5
+
+@export_group("Research")
+# The type of research that has to be done to unlock said part
+@export var researchRequired:researchGoal
+# If the part is already unlocked at start of new game
+@export var unlockedAtBeginning:bool = true
+
+
+func doTowerStuff(delta:float):
+	pass
---
+++ nodes/resources/towerResearchResources/researchResource.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name researchGoal
+
+@export var unlocked:bool = false
