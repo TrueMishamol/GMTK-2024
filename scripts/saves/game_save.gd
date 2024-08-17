8a1252ff74b80970b62193424300aa4adfe44837
Mikhail Molodozhenov (Mishamol) <reg+github@mishamol.ru>
2024-08-17 15:52:25 +0300

    UI - Resources + Main Menu Background + Camera Shake

--- scripts/saves/game_save.gd
+++ scripts/saves/game_save.gd
@@ -8,6 +8,15 @@ signal on_game_save
 
 @export var day_time: float = 0 #! Or maybe @export_range(0, 12, .05)
 
+@export var people: int = 10
+@export var potato: int = 30
+@export var carrot: int = 30
+@export var onion: int = 30
+@export var herbs: int = 0
+@export var metal: int = 0
+@export var research: int = 100
+@export var money: int = 100
+
 
 func save() -> void:
 	ResourceSaver.save(self, GAME_SAVE_PATH)
